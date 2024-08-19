bfc0ae2f851c7b1669533fda18b44ac3e15c0967
BegaliyevXushnud <xushnudpubgmn1@gmail.com>
2024-08-19 06:56:04 +0500

    first commit

---
+++ pr.detal.js
@@ -0,0 +1,16 @@
+const urlParams = new URLSearchParams(window.location.search);
+const productId = urlParams.get('id');
+
+fetch(`https://fakestoreapi.com/products/${productId}`)
+    .then(response => response.json())
+    .then(product => {
+        const productDetail = document.getElementById('product-detail');
+        const productHTML = `
+            <img src="${product.image}" alt="${product.title}">
+            <h1>${product.title}</h1>
+            <p>$${product.price}</p>
+            <p>${product.description}</p>
+        `;
+        productDetail.innerHTML = productHTML;
+    })
+    .catch(error => console.error('Error:', error));
